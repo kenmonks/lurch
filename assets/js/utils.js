aedfd0bc01a65bc43d3c970daaf6385af1bdae6d
Ken Monks <ken.monks@gmail.com>
2025-02-06 18:19:31 -0700

    Adding infrastructure for a TTD list.

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -30,13 +30,13 @@ document.addEventListener('click', (event) => {
 
 // enable clicking on the completed rows in the 100 theorem table
 document.addEventListener('DOMContentLoaded', function () {
-  const rows = document.querySelectorAll('tr[data-href]'); // Select only rows with data-href
+  const rows = document.querySelectorAll('tr[data-href]') // Select only rows with data-href
   rows.forEach(row => {
     row.addEventListener('click', function () {
-      const url = this.getAttribute('data-href');
+      const url = this.getAttribute('data-href')
       if (url) {
         openLurch(url)
-        // window.location.href = url; // Navigate to the URL
+        // window.location.href = url // Navigate to the URL
       }
     })
   })
@@ -45,14 +45,40 @@ document.addEventListener('DOMContentLoaded', function () {
 // make submenus collapsible
 document.querySelectorAll('.has-submenu').forEach(item => {
   item.addEventListener('click', () => {
-    const submenu = item.nextElementSibling;
-    const icon = item.querySelector('i');
+    const submenu = item.nextElementSibling
+    const icon = item.querySelector('i')
 
     // Toggle submenu visibility
-    submenu.classList.toggle('hidden');
+    submenu.classList.toggle('hidden')
 
     // Toggle icon class
-    icon.classList.toggle('fa-caret-down');
-    icon.classList.toggle('fa-caret-right');
-  });
-});
+    icon.classList.toggle('fa-caret-down')
+    icon.classList.toggle('fa-caret-right')
+  })
+})
+
+// Function to load the state of checkboxes from localStorage
+const loadCheckboxState = () => {
+  document.querySelectorAll('.contains-task-list input[type="checkbox"]')
+          .forEach( (checkbox,n) => {
+    checkbox.id = `checkbox-${n}`
+    const isChecked = localStorage.getItem(checkbox.id) === 'true'
+    checkbox.checked = isChecked
+    checkbox.parentElement.classList.toggle('checked', isChecked)
+  })
+}
+
+// Function to save the state of checkboxes to localStorage
+const saveCheckboxState =  (event) => {
+  const checkbox = event.target
+  localStorage.setItem(checkbox.id, checkbox.checked)
+  checkbox.parentElement.classList.toggle('checked', checkbox.checked)
+}
+
+// Attach change event listeners to all checkboxes
+document.querySelectorAll('.contains-task-list input[type="checkbox"]').forEach(checkbox => {
+  checkbox.addEventListener('change', saveCheckboxState)
+})
+
+// Load the checkbox state when the page loads
+loadCheckboxState()
